f78c554c68d9318ac433f9ce4e9cba8887de0b56
RohitPawar2707 <rohitpawar7058@gmail.com>
2025-06-28 17:01:31 +0530

    solve some pattern problems

--- pattern.java
+++ pattern.java
@@ -45,7 +45,7 @@
 
 // }
 
-// Print the (Half Pyramid)
+// Q.3 Print the (Half Pyramid)
 
 
 
@@ -65,7 +65,7 @@
 //     }
 // }
 
-// Print the pattern ( inverted Half Pyramid )
+// Q.4 Print the pattern ( inverted Half Pyramid )
 
 // public class pattern {
 //     public static void main (String args [] ){
@@ -82,4 +82,79 @@
 //     }
 // }
 
-// Print the pattern Inverted half pyramid (Rotated by 180 degree)
+// Q.5 Print the pattern Inverted half pyramid (Rotated by 180 degree)
+
+// / public class pattern{
+//     public static void main(String[] args) {
+        
+//         int n=4;
+        
+//         //Outer loop 
+//         for (int i=1; i<=n; i++){
+
+//             //Inner loop for printing the space
+//             for (int j=1; j<=n-i; j++){
+//                 System.out.print(" ");
+//             }
+//             // Inner loop for printing the *
+//             for (int j=1; j<=i; j++){
+//                 System.out.print("1");
+//             }
+//             System.out.println();
+            
+//         }
+//     }
+// }
+
+// Q.6 Print the pattern (Number Oyramid)
+
+// public class pattern {
+//     public static void main (String args []){
+
+//         int n = 5 ;
+
+//         // Outer loop
+//         for (int i=1; i<=n; i++){
+//             for (int j=1; j<=i; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Print the pyramid (Inverted by 180 degree)
+
+// public class pattern{
+//     public static void main(String[] args) {
+        
+//         int n=5;
+
+//         for (int i=1; i<=n; i++){
+//             for (int j=1; j<=n-i+1; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Q.8 Print the pattern (Floyds triangle)
+ 
+public class pattern {
+    public static void main (String args []){
+
+        int n=5;
+        int number = 1;
+
+        //Outer loop 
+        for (int i=1; i<=n; i++){ 
+            for (int j=1; j<=i; j++);
+
+            System.out.print(number);
+            number ++;
+
+        }
+        System.err.println();
+    }
+}
